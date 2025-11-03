00060b16ab729fca5dec398ca42639e6d58442dd
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-11-02 21:04:37 -0600

    correcion de errores e implementacion de navbar etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { SidebarComponent } from './partials/sidebar/sidebar.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
