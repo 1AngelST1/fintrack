afacb5df111d728a0b0ceb89e76502c608e84a50
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-11-02 19:46:00 -0600

    creacion de componentes base de partials

---
+++ src/app/partials/toast-alerts/toast-alerts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-alerts',
+  imports: [],
+  templateUrl: './toast-alerts.component.html',
+  styleUrl: './toast-alerts.component.scss'
+})
+export class ToastAlertsComponent {
+
+}
