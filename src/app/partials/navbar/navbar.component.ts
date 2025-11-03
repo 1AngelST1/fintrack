1bfad0db24f18f5d00829f485ad98b062a9341b9
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-11-03 03:28:47 -0600

    atualizacion de navbar para que en el pararesca el nombre del usuario logeado

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -1,21 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { SidebarService } from '../../services/sidebar.service';
+import { Usuario } from '../../shared/interfaces/usuario';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   sidebarService = inject(SidebarService);
+  currentUser: Usuario | null = null;
 
   constructor(
     private auth: AuthService,
     private router: Router
   ) {}
+
+  ngOnInit() {
+    this.currentUser = this.auth.getCurrentUser();
+  }
 
   onLogout() {
     this.auth.logout();
