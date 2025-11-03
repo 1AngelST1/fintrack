0724653bfe682bed14f61d07267779a70e41c8cf
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-11-02 20:39:42 -0600

    configuracion de rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Screens
+import { LandingComponent } from './screens/landing/landing.component';
+import { LoginComponent } from './screens/auth/login/login.component';
+import { RegisterComponent } from './screens/auth/register/register.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { ListComponent as CategoriesListComponent } from './screens/categories/list/list.component';
+import { FormComponent as CategoriesFormComponent } from './screens/categories/form/form.component';
+import { ListComponent as TransactionsListComponent } from './screens/transactions/list/list.component';
+import { FormComponent as TransactionsFormComponent } from './screens/transactions/form/form.component';
+import { ReportsComponent } from './screens/reports/reports.component';
+import { ProfileComponent } from './screens/profile/profile.component';
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'categories',
+    children: [
+      { path: '', component: CategoriesListComponent },
+      { path: 'form', component: CategoriesFormComponent },
+    ],
+  },
+  {
+    path: 'transactions',
+    children: [
+      { path: '', component: TransactionsListComponent },
+      { path: 'form', component: TransactionsFormComponent },
+    ],
+  },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'profile', component: ProfileComponent },
+];
