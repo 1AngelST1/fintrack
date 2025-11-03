6e461dd5029896bbda6a2cdd5f547d37d76ec019
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-11-02 21:33:35 -0600

    creacion de pantallas internas , dashboard,trasansction list y form ,categories, list y form,reports y perfile

--- src/app/screens/categories/list/list.component.ts
+++ src/app/screens/categories/list/list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
